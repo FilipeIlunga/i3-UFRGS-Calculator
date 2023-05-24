e07fa5f0f6c31bc3cbca35f21436c9106def8c01
filipeilunga <filipeilunga@gmail.com>
2023-05-24 10:49:41 -0300

    Initial Commit

---
+++ i3-Calculator/i3_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  i3_CalculatorApp.swift
+//  i3-Calculator
+//
+//  Created by Filipe Ilunga on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct i3_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
